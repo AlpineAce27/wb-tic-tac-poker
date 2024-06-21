cc5d48350293144aa62df48b1a02265d968086c3
Kimball Goss <Kimballgoss@gmail.com>
2024-06-21 13:15:12 -0600

    part 2 started

---
+++ Part2/index.js
@@ -0,0 +1,9 @@
+console.log("part 2 script attatched")
+
+let cardID = document.querySelector("#card-id") 
+let cardStyle = document.querySelector("#card-style")
+let editButton = document.querySelector("#edit-card-btn")
+
+const setCard = () => {
+    document.querySelector("#card-id".value) 
+}
