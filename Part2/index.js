07ff75372046726aa6b84e4a52a14c1aeeeeaaa3
Kimball Goss <Kimballgoss@gmail.com>
2024-06-21 13:59:07 -0600

    Finished part 2

--- Part2/index.js
+++ Part2/index.js
@@ -1,9 +1,14 @@
-console.log("part 2 script attatched")
+//console.log("part 2 script attatched")
 
 let cardID = document.querySelector("#card-id") 
 let cardStyle = document.querySelector("#card-style")
 let editButton = document.querySelector("#edit-card-btn")
 
 const setCard = () => {
-    document.querySelector("#card-id".value) 
+    let card = document.querySelector(`#${cardID.value}`)
+    //console.log(card) 
+    card.style.color = document.querySelector(`#card-style`).value
+    //console.log(card.style.color)
 }
+
+document.querySelector("#edit-card-btn").addEventListener(`click`, setCard)
